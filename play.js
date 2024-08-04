e6566439b8a0336692abcf09767cfa35c72f94dd
NourMohamed678 <nourhan.mohamed.2242002@gmail.com>
2024-08-04 10:05:01 +0300

    Add files via upload

---
+++ play.js
@@ -0,0 +1,38 @@
+
+let players1 = ["Neuer", "Pavard", "Matinez", "Alaba", "Davies", "Kimich", "Goretzka", "Coman", "Muller", "Gnarby", "Leuwandowski"]
+
+
+let players2 = ["Burki", "Schulz", "Hummels", "Akanji", "Hummels", "Weigi", "Witsel", "Hazard", "Brandt", "Sancho", "Gotze" ]
+
+
+// let BayernMunich = ["gk", ]
+
+let fieldplayers = ["Pavard", "Matinez", "Alaba", "Davies", "Kimich", "Goretzka", "Coman", "Muller", "Gnarby", "Leuwandowski"]
+
+let allPlayers = ["Neuer", "Pavard", "Matinez", "Alaba", "Davies", "Kimich", "Goretzka", "Coman", "Muller", "Gnarby", "Leuwandowski", "Burki", "Schulz", "Hummels", "Akanji", "Hummels", "Weigi", "Witsel", "Hazard", "Brandt", "Sancho", "Gotze"]
+
+let players1Final = ["Neuer", "Pavard", "Matinez", "Alaba", "Davies", "Kimich", "Goretzka", "Coman", "Muller", "Gnarby", "Leuwandowski" , "Thiago", "Coutinho", "Perisic"]
+
+let object = {
+    team1 : 1.33,
+    x: 3.25,
+    team2 : 6.5,
+}
+
+
+for(let i = 0; i>fieldplayers.length; i++){
+    console.log()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
